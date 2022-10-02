5ef718120aec3cd869608e6cf3dbca5a13f478a2
Egor Vorontsov <sdoregor@sdore.me>
2022-10-02 06:14:42 +0300

    Added `StoppableThread` and `noopcm`.

--- utils.py
+++ utils.py
@@ -2106,6 +2106,19 @@ class IndexedMeta(type):
 		#cls.__metaclass__ = metacls  # inheritance?
 		return cls
 
+class StoppableThread(threading.Thread):
+	def __init__(self,  *args, kwargs=None, **kwargs_):
+		_stop_event = self._stop_event = threading.Event()
+		if (kwargs is None): kwargs = dict()
+		kwargs['_stop_event'] = _stop_event
+		super().__init__(*args, kwargs=kwargs, **kwargs_)
+
+	def stop(self):
+		self._stop_event.set()
+
+	def is_stopped(self):
+		return self._stop_event.is_set()
+
 @funcdecorator
 def instantiate(f):
 	""" Decorator that replaces type returned by decorated function with instance of that type and leaves it as is if it is not a type. """
@@ -2133,6 +2146,13 @@ class noopf:
 
 	def __bool__(self):
 		return False
+@singleton
+class noopcm:
+	def __enter__(*_):
+		pass
+
+	def __exit__(*_):
+		pass
 
 @cachedfunction
 def getsubparser(ap, *args, **kwargs): return ap.add_subparsers(*args, **kwargs)
